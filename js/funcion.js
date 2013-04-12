ddfbf6792d33c59f878b951b3b5ade49d1f0d8ab
dehivis <dehivis.oliver@gmail.com>
2013-04-12 00:36:30 -0430

    primer commit

---
+++ js/funcion.js
@@ -0,0 +1,61 @@
+// JavaScript Document
+
+function showLightbox() {
+	document.getElementById('over').style.display='block';
+	document.getElementById('fade').style.display='block';	
+	}
+
+function hideLightbox() {
+	document.getElementById('over').style.display='none';
+	document.getElementById('fade').style.display='none';
+	}
+
+function showLightbox2() {
+	document.getElementById('over2').style.display='block';
+	document.getElementById('fade2').style.display='block';	
+	}
+
+function hideLightbox2() {
+	document.getElementById('over2').style.display='none';
+	document.getElementById('fade2').style.display='none';
+	}
+
+function showLightbox3() {
+	document.getElementById('over3').style.display='block';
+	document.getElementById('fade3').style.display='block';	
+	}
+
+function hideLightbox3() {
+	document.getElementById('over3').style.display='none';
+	document.getElementById('fade3').style.display='none';
+	}
+
+function showLightbox4() {
+	document.getElementById('over4').style.display='block';
+	document.getElementById('fade4').style.display='block';	
+	}
+
+function hideLightbox4() {
+	document.getElementById('over4').style.display='none';
+	document.getElementById('fade4').style.display='none';
+	}
+
+function showLightbox5() {
+	document.getElementById('over5').style.display='block';
+	document.getElementById('fade5').style.display='block';	
+	}
+
+function hideLightbox5() {
+	document.getElementById('over5').style.display='none';
+	document.getElementById('fade5').style.display='none';
+	}
+	
+function showLightbox6() {
+	document.getElementById('over6').style.display='block';
+	document.getElementById('fade6').style.display='block';	
+	}
+
+function hideLightbox6() {
+	document.getElementById('over6').style.display='none';
+	document.getElementById('fade6').style.display='none';
+	}
